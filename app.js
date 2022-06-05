5e72f13f8a0dc543d6e5bc7b7e90eefc6bc4ac19
Poppy22 <carmengpopa13@gmail.com>
2022-06-05 16:43:32 +0300

    prepared for deployment

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(middleware.tokenExtractor)
+app.use(express.static("UI"));
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
